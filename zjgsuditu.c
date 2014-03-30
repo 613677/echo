6ee048d075570c60ed4aa58d7cf2ab9ea1f6efb5
613677 <echo.jinjing@gmail.com>
2014-03-30 18:55:29 +0800

    Create zjgsuditu.c
    ·给定景点编号
    ·给相邻两景点定义最短距离用一个算法表示
    ·该项目主要用于景点简介及算出指定景点到其他景点的最短距离
    ·初定用Dijkstra算法实现

---
+++ zjgsuditu.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+typedef struct ZJGSU   /*定义结构体所包含的景点代码、名称、简介*/
+{            
+     int  code;
+     char name[num];
+     char pr[num];
+}ZJGSU; 
+int main()
+{
+   return 0;
+}
+
+   
