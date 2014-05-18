7304fcecd37af91ca4bcbfb742555a81ac40420a
613677 <echo.jinjing@gmail.com>
2014-05-18 19:40:21 +0800

    Update zjgsuditu.c

--- zjgsuditu.c
+++ zjgsuditu.c
@@ -3,9 +3,9 @@
 #include<math.h>
 typedef struct ZJGSU   /*定义结构体所包含的景点代码、名称、简介*/
 {            
-     int  code;
-     char name[num];
-     char pr[num];
+     int  code;//景点代号
+     char name[num];//景点名称
+     char pr[num];//景点简介
 }ZJGSU; 
 int main()
 {
